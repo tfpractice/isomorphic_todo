0d6a1da0f556173da520f676ad4cf5b8f7b95796
theory friction practice <tfpractice@theorys-MacBook-Air.local>
2016-11-05 13:59:23 -0400

    requestHandler

--- server/app.js
+++ server/app.js
@@ -1,16 +1,8 @@
 import express from 'express';
-import React from 'react';
-import { renderToString } from 'react-dom/server'
-import { RoutingContext, match } from 'react-router';
-import createLocation from 'history/lib/createLocation';
-import routes from 'routes';
-import { Provider } from 'react-redux';
-import * as reducers from 'reducers';
-import promiseMiddleware from '../shared/lib/promiseMiddleware';
-import fetchComponentData from '../shared/lib/fetchComponentData';
-import { createStore, combineReducers, applyMiddleware } from 'redux';
+
 import path from 'path';
-// import mongoose from 'mongoose';
+import { handleRequest } from './requestHandler';
+
 const app = express();
 
 if (process.env.NODE_ENV !== 'production') {
@@ -18,60 +10,6 @@ if (process.env.NODE_ENV !== 'production') {
 }
 
 app.use(express.static(path.join(__dirname, 'dist')));
-
-app.use((req, res) => {
-  const location = createLocation(req.url);
-  const reducer = combineReducers(reducers);
-  const store = applyMiddleware(promiseMiddleware)(createStore)(reducer);
-
-  match({ routes, location }, (err, redirectLocation, renderProps) => {
-    if (err) {
-      console.error(err);
-      return res.status(500).end('Internal server error');
-    }
-
-    if (!renderProps)
-      return res.status(404).end('Not found');
-
-    function renderView() {
-      const InitialView = (
-        <Provider store={store}>
-          <RoutingContext {...renderProps} />
-        </Provider>
-      );
-
-      const componentHTML = renderToString(InitialView);
-
-      const initialState = store.getState();
-
-      const HTML =
-        `
-      <!DOCTYPE html>
-      <html>
-        <head>
-          <meta charset="utf-8">
-          <title>Redux Demo</title>
-
-          <script>
-            window.__INITIAL_STATE__ = ${JSON.stringify(initialState)};
-          </script>
-        </head>
-        <body>
-          <div id="react-view">${componentHTML}</div>
-          <script type="application/javascript" src="/bundle.js"></script>
-        </body>
-      </html>
-      `;
-
-      return HTML;
-    }
-
-    fetchComponentData(store.dispatch, renderProps.components,
-        renderProps.params)
-      .then(renderView)
-      .then(html => res.end(html))
-      .catch(err => res.end(err.message));
-  });
-});
+app.use(handleRequest)
 
 export default app;
