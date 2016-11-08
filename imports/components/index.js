b2d6588c3efa22cef0c154558af3bc7a55ca4ffd
theory friction practice <jackson.brebnor@gmail.com>
2016-11-07 21:54:51 -0500

    children was a propety of an object not a simple param

--- imports/components/index.js
+++ imports/components/index.js
@@ -1,32 +1,15 @@
 import React, { PropTypes } from 'react';
 
-// const Main = (children) => (
-//     <div id="main-view">
-//         <h1>Todos</h1>
-
-//         <hr />
-
-//         {children}
-
-//       </div>
-// );
-
-// Main.propTypes = { children: PropTypes.object };
-// export default Main;
-export default class MainView extends React.Component {
-	static propTypes = {
-		children: PropTypes.object
-	};
-
-	render() {
-		return (
-			<div id="main-view">
+const Main = ({ children }) => (
+	<div id="main-view">
         <h1>Todos</h1>
 
         <hr />
 
-        {this.props.children}
+        {children}
+
       </div>
-		);
-	}
-}
+);
+
+Main.propTypes = { children: PropTypes.object };
+export default Main;
