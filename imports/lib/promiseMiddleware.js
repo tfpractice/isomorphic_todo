192dbc36d7ef015dad5d39bcb4bca81ee8981d15
theory friction practice <jackson.brebnor@gmail.com>
2016-11-07 22:00:24 -0500

    logging middleware

--- imports/lib/promiseMiddleware.js
+++ imports/lib/promiseMiddleware.js
@@ -1,27 +1,28 @@
 export default function promiseMiddleware() {
-  return next => action => {
-    const { promise, type, ...rest } = action;
+	return next => action => {
+		const { promise, type, ...rest } = action;
 
-    if (!promise) return next(action);
+		if (!promise) return next(action);
 
-    const SUCCESS = type;
+		const SUCCESS = type;
+		const REQUEST = type + '_REQUEST';
+		const FAILURE = type + '_FAILURE';
 
-    const REQUEST = type + '_REQUEST';
-    const FAILURE = type + '_FAILURE';
+		next({...rest, type: REQUEST });
 
-    next({ ...rest, type: REQUEST });
+		return promise
+			.then(res => {
+				console.log('action', action);
+				console.log('next', next);
+				next({...rest, res, type: SUCCESS });
 
-    return promise
-      .then(res => {
-        next({ ...rest, res, type: SUCCESS });
+				return true;
+			})
+			.catch(error => {
+				next({...rest, error, type: FAILURE });
+				console.log(error);
 
-        return true;
-      })
-      .catch(error => {
-        next({ ...rest, error, type: FAILURE });
-        console.log(error);
-
-        return false;
-      });
-  };
+				return false;
+			});
+	};
 }
