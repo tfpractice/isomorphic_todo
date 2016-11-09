6c2ca2b6d27c55df8540581573130016b9feb323
theory friction practice <tfpractice@theorys-MacBook-Air.local>
2016-11-08 19:27:15 -0500

    container

--- imports/components/main.js
+++ imports/components/main.js
@@ -1,7 +1,7 @@
 import React, { PropTypes } from 'react';
 
 const Main = ({ children }) => (
-    <div id="main-view">
+	<div id="main-view" className='container'>
         <h1>Todos</h1>
 
         <hr />
