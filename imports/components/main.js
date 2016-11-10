7602be5a3de0684451dbbdcec2f15f5a844ab695
theory friction practice <jackson.brebnor@gmail.com>
2016-11-10 16:09:43 -0500

    imported Material Ui weird errors

--- imports/components/main.js
+++ imports/components/main.js
@@ -1,17 +1,24 @@
 import React, { Component, PropTypes } from 'react';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import RaisedButton from 'material-ui/RaisedButton';
 
-export default class Main extends Component {
-    render() {
-        return (
-          <div id="main-view" className='container'>
+class Main extends Component {
+	render() {
+		return (
+			<div id="main-view" className='container'>
+			 	<RaisedButton label="TodoButton" />
                 <h1>Todos</h1>
                 <hr/>
                 {this.props.children}
           </div>
-          );
-    }
-}
-;
+		);
+	}
+};
 
+const MainMui = () => (
+	<MuiThemeProvider>
+      <Main />
+    </MuiThemeProvider>
+);
 // Main.propTypes = { children: PropTypes.object, };
-// export default Main;
+export default MainMui;
