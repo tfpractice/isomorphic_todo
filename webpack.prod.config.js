9a140f4444f5a3a7f5bca5333c357af65e33fe6a
theory friction practice <jackson.brebnor@gmail.com>
2016-11-16 15:04:31 -0500

    formatting conf

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -4,9 +4,8 @@ var webpack = require('webpack');
 module.exports = { entry: [
     './client',
   ],
-  // resolve: { modulesDirectories: [ 'node_modules', 'imports' ],
   resolve: { modulesDirectories: ['node_modules', ],
-    extensions: [ '', '.js', '.jsx' ], },
+    extensions: ['', '.js', '.jsx' ], },
   output: { path: path.join(__dirname, 'dist'),
     filename: 'bundle.js',
     publicPath: '/', },
