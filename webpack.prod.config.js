aa259ea20b62bedee61094416b800bc397e66bf0
theory friction practice <jackson.brebnor@gmail.com>
2016-11-16 13:18:15 -0500

    conf

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -1,35 +1,23 @@
 var path = require('path');
 var webpack = require('webpack');
 
-module.exports = {
-	entry: [
-		'./client',
-	],
-	resolve: {
-		modulesDirectories: ['node_modules', 'imports'],
-		extensions: ['', '.js', '.jsx'],
-	},
-	output: {
-		path: path.join(__dirname, 'dist'),
-		filename: 'bundle.js',
-		publicPath: '/',
-	},
-	module: {
-		loaders: [{
-			test: /\.jsx?$/,
-			exclude: /node_modules/,
-			loaders: ['babel'],
-		}, ],
-	},
-	plugins: [
-		new webpack.DefinePlugin({ 'process.env': { NODE_ENV: '"production"', }, }),
-		new webpack.optimize.UglifyJsPlugin({ compress: { warnings: false, }, }),
-	],
-	node: {
-		fs: 'empty',
-		net: 'mock',
-		tls: 'mock',
-		dns: 'mock',
-		net: 'mock',
-	},
-};
+module.exports = { entry: [
+    './client',
+  ],
+  resolve: { modulesDirectories: ['node_modules', 'imports'],
+    extensions: ['', '.js', '.jsx'], },
+  output: { path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/', },
+  module: { loaders: [{ test: /\.jsx?$/,
+      exclude: /node_modules/,
+      loaders: [ 'babel' ], },], },
+  plugins: [
+    new webpack.DefinePlugin({ 'process.env': { NODE_ENV: '"production"', }, }),
+    new webpack.optimize.UglifyJsPlugin({ compress: { warnings: false, }, }),
+  ],
+  node: { fs: 'empty',
+    net: 'mock',
+    tls: 'mock',
+    dns: 'mock',
+    net: 'mock', }, };
