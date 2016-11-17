499fad9dfd085f991102e37cee893674f7ba305b
theory friction practice <jackson.brebnor@gmail.com>
2016-11-17 12:10:26 -0500

    cleanup on actions file

--- imports/tasks/actions.js
+++ imports/tasks/actions.js
@@ -23,11 +23,8 @@ const failure = () => TASK_REQUEST_FAILURE;
 
 const update = (newTasks) => (tState) => newTasks;
 const insert = (task) => (tasks) => tasks.concat(task);
-const edit = (task) => (tasks = []) => {
-  console.log('TAKS TO EDIT', task);
-  console.log('TAKS TO EDIT ID', task.id);
-  return tasks.map(t => t.id === task.id ? { ...t, ...task } : t);
-};
+const edit = (task) => (tasks = []) =>
+  tasks.map(t => t.id === task.id ? { ...t, ...task } : t);
 
 export const taskRequestSucess = ({ data: { tasks } }) => (dispatch) => {
     dispatch({ type: TASK_REQUEST_SUCCESS, curry: success });
@@ -48,10 +45,10 @@ export const getTasks = () => (dispatch) => {
 export const insertTask = (task) =>
  ({ type: INSERT_TASK, curry: insert(task), });
 
-export const createTask = (taskProps) => (dispatch) =>	{
-    return axios.post(`${API_URL}/tasks`, taskProps)
+export const createTask = (taskProps) => (dispatch) =>
+     axios.post(`${API_URL}/tasks`, taskProps)
     .then(({ data:{ task } })=> dispatch(insertTask(task)))
-    .catch(err => console.error('there was an error in creation', err));};
+    .catch(err => console.error('there was an error in creation', err));
 
 export const updateTasks = (tasks) => {
     return ({ type: UPDATE_TASKS, curry: update(tasks), });};
@@ -59,17 +56,10 @@ export const updateTasks = (tasks) => {
 export const updateTask = (task)=>
   ({ type: EDIT_TASK, curry: edit(task) });
 
-export const editTask = ({ id, cuid })=>(dispatch)=> (taskProps) => {
-  console.log('======= ID AND CUID======', id, cuid);
-  console.log('======= TASK PROPS======', taskProps);
-  return axios.patch(`${API_URL}/tasks/${id}`, taskProps)
-  .then(({ data:{ task } })=> {
-    console.log('======= TASK PROPS======', task);
-    console.log('======= TASK PROPS.ID======', task.id);
-    return dispatch(updateTask(task));
-  })
+export const editTask = ({ id, cuid })=>(dispatch)=> (taskProps) =>
+   axios.patch(`${API_URL}/tasks/${id}`, taskProps)
+  .then(({ data:{ task } })=> dispatch(updateTask(task)))
   .catch(err => console.error('there was an error in update', err));
-};
 
 export const deleteTask = (id) =>
   ({ type: DELETE_TASK, id, });
