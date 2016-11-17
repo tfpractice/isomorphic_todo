9d9dd6791c595292ab9d40f2ba7b9e77b7a5582a
theory friction practice <jackson.brebnor@gmail.com>
2016-11-16 20:38:30 -0500

    failed chunking

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -1,26 +1,39 @@
 // var path = require('path');
 const validate = require('webpack-validator');
-const{ resolve, join } = require('path');
-var webpack = require('webpack');
+const{ resolve } = require('path');
+const webpack = require('webpack');
+
+const pkg = require('./package.json');
+
+// ...
+
+// const common = {
+//   entry: {
+//     app: PATHS.app,
+//     vendor: Object.keys(pkg.dependencies)
+//   },
+  // ...
 const ROOT_PATH = resolve(__dirname);
 console.log('============ROOT_PATH=============');
 console.log(ROOT_PATH);
 
-module.exports = validate({ context: ROOT_PATH, entry: [
-    './client',
-  ],
-  resolve: { modulesDirectories: ['node_modules', ],
-    extensions: ['', '.js', '.jsx'], },
+module.exports = validate({ context: ROOT_PATH,
+  entry:  './client',
+    // vendor: Object.keys(pkg.dependencies), },
+  resolve: { modulesDirectories: ['node_modules',],
+    extensions: [ '', '.js', '.jsx' ], },
   output: { path: resolve(ROOT_PATH, 'dist'),
-    filename: 'bundle.js',
+    filename:  'bundle.js',
     publicPath: '/', },
   module: { loaders: [{ test: /\.jsx?$/,
       exclude: /node_modules/,
-      loaders: [ 'babel' ], }, { test: /\.css$/,
-        loaders: ['style', 'css'], }, ], },
+      loaders: ['babel'], }, { test: /\.css$/,
+        loaders: [ 'style', 'css' ], }, ], },
   plugins: [
     new webpack.DefinePlugin({ 'process.env': { NODE_ENV: '"production"', }, }),
     new webpack.optimize.UglifyJsPlugin({ compress: { warnings: false, }, }),
+    // new webpack.optimize.CommonsChunkPlugin({ name: 'vendor' }),
+
   ],
   node: { fs: 'empty',
     net: 'mock',
