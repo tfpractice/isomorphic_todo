7a856ca573b049799d24bb7b47590c9c10c6246c
theory friction practice <jackson.brebnor@gmail.com>
2016-11-17 12:04:32 -0500

    abstracts taskform

--- imports/tasks/components/form.js
+++ imports/tasks/components/form.js
@@ -1,54 +1,16 @@
 import React, { PropTypes } from 'react';
 import { reduxForm, Field } from 'redux-form';
-import { connect } from 'react-redux';
-
 import FlatButton from 'material-ui/FlatButton';
-;
-import {
-  Checkbox,
-  TextField,
-  Toggle
-} from 'redux-form-material-ui';
+import { Checkbox,  TextField,  Toggle } from 'redux-form-material-ui';
 
-const TaskFormF = ({ handleSubmit,  dispatch, ...props }) =>  {
-  console.log('I am the new form',  props);
-  console.log('I am the new form',  props.clearSubmit.toString());
-  return (
-
-
-
-<form onSubmit={(v)=> {
-  console.log('VALUES', v);handleSubmit(v);
-  // console.log('VALUES', v);handleSubmit(v);props.reset(props.form);
-  }} >
-  <Field name="title" component={TextField} hintText="What task"/>
-  <Field name="description" component={TextField} hintText="What about it"/>
-    <Field name="private" component={Toggle} label="Private?"/>
-    <Field name="completed" component={Checkbox} label="Is it done?"/>
-    <FlatButton label="submit" type="submit"  />
-  </form>
-);
-};
-
-const EditFormF = ({ handleSubmit, task, dispatch, ...props }) =>  {
-  console.log('I am edit form', task, props);
-  return (
-
-
-
-<form onSubmit={(v)=> {
-    console.log('VALUES', v); handleSubmit(v);props.reset(props.form);
-  }}>
+const TaskForm = ({ handleSubmit }) =>   (
+  <form onSubmit={handleSubmit}>
     <Field name="title" component={TextField} hintText="What task"/>
     <Field name="description" component={TextField} hintText="What about it"/>
     <Field name="private" component={Toggle} label="Private?"/>
     <Field name="completed" component={Checkbox} label="Is it done?"/>
     <FlatButton label="submit" type="submit"  />
   </form>
-);};
+);
 
-const TaskForm = reduxForm({ form: 'TaskForm' })(TaskFormF);
-export const EditForm = reduxForm()(EditFormF);
-const mapStateToProps = (state, { task })=>({ form: `edit_form${task.id}` });
-export const EditForm2 = connect(mapStateToProps)(EditFormF);
-export default TaskForm;
+export default reduxForm()(TaskForm);
--- imports/tasks/components/tasks_view.js
+++ imports/tasks/components/tasks_view.js
@@ -1,28 +1,25 @@
 import React, { PropTypes } from 'react';
 import FlatButton from 'material-ui/FlatButton';
 import { reset } from 'redux-form';
-
-import TaskForm, { EditForm } from './form';
-
+import TaskForm from './form';
+const resetForm = (name)=>(action, dispatch)=> dispatch(reset(name));
 const TasksView = ({ tasks, actions }) => {
-  console.log(EditForm);
   return (
 
-
-
 <div className="tasks-list">
-  <TaskForm  onSubmit	={actions.createTask} onSubmitSuccess={(arg, dispatch)=> {
-    console.log('SUBMISSION ARG', arg);
-    console.log('SUBMISSION REST', dispatch);
-    return dispatch(reset('TaskForm'));
-  }}/>
-    <FlatButton label="TASKS" onClick={actions.fetchTasks}/>
-    {tasks.map((task, index) => (
-      <div key={index}>
-        <span>{task.title}</span>
-        <FlatButton label="Delete" data-id={index} />
-        <FlatButton label="Edit" data-id={index} />
-        <EditForm key={task.id} form={ `edit_form${task.id}` } task={task} onSubmit={actions.editTask(task)}/>
+  <TaskForm  form={'newTaskForm'}
+    onSubmit={actions.createTask}
+    onSubmitSuccess={resetForm('newTaskForm')}/>
+  {tasks.map((task, index) => (
+    <div key={index}>
+      <span>{task.title}</span>
+      <FlatButton label="Delete" data-id={index} />
+      <FlatButton label="Edit" data-id={index} />
+      <TaskForm key={task.id}
+        form={ `edit_form${task.id}` }
+        initialValues={task}
+        onSubmit={actions.editTask(task)}
+        onSubmitSuccess={resetForm(`edit_form${task.id}`)}/>
       </div>
     ))}
   </div>);};
