c994afe53f04244a2038304bacf1a8fd71f54b62
theory friction practice <jackson.brebnor@gmail.com>
2016-11-17 19:57:55 -0500

    passport setup successful

--- server/app.js
+++ server/app.js
@@ -6,31 +6,59 @@ import mongoose from 'mongoose';
 import { server as serverConfig } from '../config';
 import { TaskRoutes } from './routes';
 import { handleRequest } from './requestHandler';
-
+import cookieParser from 'cookie-parser';
+import session from 'express-session';
+import flash from 'express-flash';
+import passport from 'passport';
+import localStrategy from 'passport-local';
+import expressValidator from 'express-validator';
 const app = express();
 if (process.env.NODE_ENV !== 'production') {
-	require('../webpack.dev').default(app);
+  require('../webpack.dev').default(app);
 }
 
 mongoose.Promise = global.Promise;
 
 // MongoDB Connection
 mongoose.connect(serverConfig.mongoURL, (error) => {
-	if (error) {
-		console.error(
-			'Please make sure Mongodb is installed and running!'); // eslint-disable-line no-console
-		throw error;
-	}
-
-	console.log('mongoose connected');
+  if (error) {
+    console.error(
+      'Please make sure Mongodb is installed and running!'); // eslint-disable-line no-console
+    throw error;
+  }
+  
+  console.log('mongoose connected');
 });
 
 app.use(bodyParser.json({ limit: '20mb' }));
 app.use(bodyParser.urlencoded({ limit: '20mb', extended: false }));
+app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'dist')));
+app.use(session({ secret: 'secret', saveUninitialized: true, resave: true }));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(expressValidator({ errorFormatter: function (param, msg, value){
+      var namespace = param.split('.'), root    = namespace.shift(), formParam = root;
+      
+      while (namespace.length) {
+        formParam += '[' + namespace.shift() + ']';
+      }
+      
+      return { param: formParam,
+          msg: msg,
+          value: value, };
+    }, }));
+
+app.use(flash());
+app.use((req, res, next)=> {
+  res.locals.error = req.flash('error ');
+  res.locals.error_msg = req.flash('error message');
+  res.locals.success_msg = req.flash('success message');
+  next();
+});
 app.use((req, res, next) => {
-	global.navigator = { userAgent: 'all' };
-	next();
+  global.navigator = { userAgent: 'all' };
+  next();
 });
 app.use('/api', TaskRoutes);
 app.use(handleRequest);
